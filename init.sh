e54c63ba2dad75e6ad7c68d18d40e308f6559557
M.R.Z <zgd1348833@gmail.com>
2020-01-03 13:15:07 +0100

    add powerline fonts

--- init.sh
+++ init.sh
@@ -10,7 +10,8 @@ BASH_URL="https://github.com/GdZ/oh-my-bash.git"
 BASH_LOCAL="oh-my-bash"
 VIM_URL="https://github.com/GdZ/spf13-vim.git"
 VIM_LOCAL="spf13-vim-3"
-
+FONT_URL="https://github.com/powerline/fonts.git"
+FONT_LOCAL="fonts/powerline"
 ## task list
 doTask(){
     echo git clone ${ENV_URL} ${ENV_LOCAL}
@@ -23,6 +24,8 @@ doTask(){
     git clone ${BASH_URL} ${BASH_LOCAL}
     echo git clone ${VIM_URL} ${VIM_LOCAL}
     git clone ${VIM_URL} ${VIM_LOCAL}
+    echo git clone ${FONT_URL} ${FONT_LOCAL}
+    git clone ${FONT_URL} ${FONT_LOCAL}
 }
 
 ## just do it
